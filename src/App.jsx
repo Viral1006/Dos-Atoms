0756e999e6f0b42637b429994df199596b07f845
Nemeziz10 <parikhviral02@gmail.com>
2024-05-30 14:50:44 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import {motion} from 'framer-motion';
+import './index.css'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import About from './components/About'
+import './fonts.css';
+
+function App() {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0
+  })
+  
+  const [cursorVariant, setCursorVariant] = useState("default");
+
+  // console.log(mousePosition);
+
+  useEffect(() => {
+   const mouseMove = e => {
+    setMousePosition({
+      x: e.clientX,
+      y: e.clientY
+    })
+   }
+
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove)
+    }
+  }, []);
+
+
+  const variants = {
+    default: {
+      x: mousePosition.x - 16,
+      y: mousePosition.y - 16,
+      transition: {
+        type: "smooth",
+        duration: 0,
+      },
+      backgroundColor: "#f3994c",
+      mixBlendMode: "difference",
+      text:{
+       x: mousePosition.x - 75,
+       y: mousePosition.y - 75,
+       height: 150,
+       width: 150,
+       backgroundColor: "yellow",
+       mixBlendMode: "difference"
+        
+      }
+    }
+  }
+
+  // const textEnter = () => setCursorVariant("text");
+  // const textLeave = () => setCursorVariant("default");
+
+  return (
+    <>
+      <div>
+        <motion.div
+        className= ' cursor '
+        variants={variants}
+        animate = {cursorVariant}
+        />
+        <Navbar />
+        <Home />
+        <About />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ArrowRight from 'D:/corporate-web/src/assets/ArrowRight.svg';
+
+const Navbar = () => {
+  return (
+    <div className="relative h-[37px] bg-[#f1f1ea] grainy overflow-x-hidden overflow-y-hidden">
+        <div className="relative"> 
+        <div className='absolute top-2 left-[64px] w-4 h-4 bg-[#515151] rounded-full'></div>
+        <div className='absolute top-2 left-[40px] w-4 h-4 bg-[#101110] rounded-full'></div>
+      </div>
+      <div className="absolute top-1 right-0 flex items-center pr-4">
+
+    <button className="bg-none text-base text-[#101110] px-2 py-1 mr-[100px]">EN</button>
+
+    <button className="bg-none text-base text-[#101110] mr-[100px] flex items-center">
+      CONTACT US
+      {/* <svg width="40" height="24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="feather feather-arrow-right" viewBox="0 0 24 24" transform="rotate(0 12 12)">
+    <line x1="5" y1="12" x2="19" y2="12"></line>
+    <polyline points="12 5 19 12 12 19"></polyline>
+    </svg> */}
+    <img src={ArrowRight} alt="Arrow Right" width="20" height="16" className="ml-2 mb-1" />
+    </button>
+
+    <div className="ml-4">
+      <div className="w-8 h-0.5 bg-[#101110] mb-1"></div>
+      <div className="w-8 h-0.5 bg-[#101110] mb-1"></div>
+    </div>
+  </div>
+    </div>
+  )
+}
+
+export default Navbar
